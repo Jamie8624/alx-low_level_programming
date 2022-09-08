9709dbc1648c9f5c8e3f01cf3173870017837c98
Olusanya Ajibola <olusanya.ajibola.beecit@gmail.com>
2022-09-08 09:42:52 -0700

    t4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - print the string in the function
+ *
+ * Description: Print string Using the main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+		return (0);
+}
+
+
