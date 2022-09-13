9f3251034a207f166455e68cc14c852902176a04
Olusanya Ajibola <olusanya.ajibola.beecit@gmail.com>
2022-09-13 11:35:46 -0700

    t0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,12 @@
  */
 int main(void)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
+	char alp[] = "_putchar;
+	int c;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
+	for (c = 0; c < sz; c++)
 	{
-		_putchar(str[count]);
+		_putchar(alp[c]);
 	}
 	_putchar('\n');
 	return (0);
